41ca64eb6f0967bcc9748606df344b4aaba5023d
Pate Blomqvist <pate.blomqvist@gmail.com>
2023-03-28 14:08:26 +0300

    Implement skeleton for Stat Test interactions

--- Procedural/GenerationLogic/FragmentUtils.cs
+++ Procedural/GenerationLogic/FragmentUtils.cs
@@ -44,6 +44,10 @@ namespace JiME.Procedural.GenerationLogic
                         CreateDialogInteraction(fragment, interactionInfo, s, random, startTrigger, endTriggers[i], location, tile);
                         break;
 
+                    case InteractionType.StatTest:
+                        CreateStatTestInteraction(fragment, interactionInfo, s, random, startTrigger, endTriggers[i], location, tile);
+                        break;
+
                     default:
                         throw new Exception("FragmentUtils: Unhandled interaction type " + interactionInfo.Type.ToString());
                 }
@@ -53,42 +57,135 @@ namespace JiME.Procedural.GenerationLogic
         #region Interaction - Dialog
         private static void CreateDialogInteraction(
             string fragmentName,
-            StoryFragment.InteractionInfo interactionInfo,
-            Scenario s, 
-            Random random, 
-            string startTrigger, 
-            string endTrigger, 
-            string location, 
+            StoryFragment.InteractionInfo info,
+            Scenario s,
+            Random random,
+            string startTrigger,
+            string endTrigger,
+            string location,
             HexTile tile)
         {
             // TODO: invent story elements from startTrigger to endTrigger based on fragment and other stuff
-            var dummySolution = new DialogInteraction(fragmentName.ToString() + " (" + location.ToString() + ")" + GenerateRandomNameSuffix());
-            dummySolution.eventBookData = new TextBookData()
+            AddTokenInteraction(s, tile, startTrigger, info, () =>
             {
-                pages = new List<string>() { "What do you want to do?" }
-            };
-            dummySolution.choice1 = "Trigger: " + endTrigger;
-            dummySolution.c1Text = "You decided to go forward";
-            dummySolution.c1Trigger = endTrigger;
-            dummySolution.choice2 = ""; // This removes the option alltogether
-            dummySolution.choice3 = ""; // This removes the option alltogether
-                                        //dummySolution.triggerName = startTrigger; // This must not be set as it causes wrong triggers to fie in the App, this is triggered only by the token
-            dummySolution.isTokenInteraction = true; // Needs to be token interaction so that it can be triggered by a token
-                                                     //dummySolution.tokenType = TokenType.Person;
-                                                     //dummySolution.personType = PersonType.Elf;
-            s.AddInteraction(dummySolution);
+                var dialog = new DialogInteraction("Dialog" + GenerateRandomNameSuffix());
+                dialog.eventBookData = new TextBookData()
+                {
+                    pages = new List<string>() { "What do you want to do?" }
+                };
+                dialog.choice1 = "Trigger: " + endTrigger;
+                dialog.c1Text = "You decided to go forward";
+                dialog.c1Trigger = endTrigger;
+                dialog.choice2 = ""; // This removes the option alltogether
+                dialog.choice3 = ""; // This removes the option alltogether
 
+                // Return for finalization
+                return dialog;
+            });
+        }
+        #endregion
+        #region Interaction - StatTest
+        private static void CreateStatTestInteraction(
+            string fragmentName,
+            StoryFragment.InteractionInfo info,
+            Scenario s,
+            Random random,
+            string startTrigger,
+            string endTrigger,
+            string location,
+            HexTile tile)
+        {
+            // TODO: invent story elements from startTrigger to endTrigger based on fragment and other stuff
 
-            var token = new Token(TokenType.Search);
-            // TODO:token position? 
-            token.triggeredByName = startTrigger; // Only reveal the token when the correct objective has been activated    
-            token.triggerName = dummySolution.dataName; // Token triggers the dialog
-            tile.tokenList.Add(token);
+            AddTokenInteraction(s, tile, startTrigger, info, () =>
+            {
+                var mainStat = info.StatHint ?? Ability.Random;
+                var statTest = new TestInteraction("Test " + mainStat.ToString() + GenerateRandomNameSuffix());
+
+                // Setup basic test
+                statTest.testAttribute = mainStat;
+                statTest.altTestAttribute = info.AltStatHint ?? Ability.Random;
+                statTest.noAlternate = info.AltStatHint == null;
+
+                // Token interaction flavor text
+                statTest.textBookData = new TextBookData()
+                {
+                    pages = new List<string>() { "Are you sure you want to start testing?" }
+                };
+                // Test main flavor text
+                statTest.eventBookData = new TextBookData()
+                {
+                    pages = new List<string>() { "This TEST will be very hard!" }
+                };
+
+                // Setup test type
+                if (info.StatTestType == StoryFragment.StatTestType.OneTry)
+                {
+                    statTest.isCumulative = false;
+                    statTest.passFail = false;
+                }
+                else if (info.StatTestType == StoryFragment.StatTestType.Retryable)
+                {
+                    statTest.isCumulative = true;
+                    statTest.passFail = true;
+                }
+                else // StoryFragment.StatTestType.Cumulative
+                {
+                    statTest.isCumulative = true;
+                    statTest.passFail = false;
+                }
+
+                // Success
+                statTest.passBookData = new TextBookData()
+                {
+                    pages = new List<string>() { "Test SUCCESSFUL, threat decreases" }
+                };
+                statTest.successValue = 4; // TDOO: where to get a good value here?
+                statTest.successTrigger = endTrigger;
+                statTest.rewardXP = 0;
+                statTest.rewardLore = 0;
+                statTest.rewardThreat = 5; // TDOO: where to get a good value here?
+
+                // Progress
+                statTest.progressBookData = new TextBookData()
+                {
+                    pages = new List<string>() { "Keep trying!" }
+                }; ;
+
+                // Failure
+                statTest.failBookData = new TextBookData()
+                {
+                    pages = new List<string>() { "Test FAILED, threat increases" }
+                }; ;
+                statTest.failTrigger = endTrigger; // Failure also progresses things for now, TODO: Perhaps add some more interactions if failed? e.g. have to fight a monster
+                statTest.failThreat = 5; // TDOO: where to get a good value here?
+                
+                return statTest;
+            });
         }
         #endregion
         #region Utils
 
         private static string GenerateRandomNameSuffix() => string.Format(" ({0})", Guid.NewGuid().GetHashCode());
+
+        private static void AddTokenInteraction<TInteraction>(Scenario s, HexTile tile, string startTrigger, StoryFragment.InteractionInfo info, Func<TInteraction> setupAction) where TInteraction : InteractionBase
+        {
+            // Create the interaction with specific setup
+            var i = setupAction();
+
+            // Finalize the interaction
+            i.triggerName = "None"; // We must not set a starting trigger since this is triggered by a Token
+            i.isTokenInteraction = true; // Needs to be token interaction so that it can be triggered by a token
+            s.AddInteraction(i); // Add the trigger itself
+
+            // Create token to trigger it
+            var token = new Token(info.TokenHint ?? TokenType.Search);
+            // TODO:token position? 
+            token.triggeredByName = startTrigger; // Only reveal the token when the correct objective has been activated    
+            token.triggerName = i.dataName; // Token triggers the dialog
+            // TODO: token flavor text?
+            tile.tokenList.Add(token);
+        }
 
         #endregion
     }
